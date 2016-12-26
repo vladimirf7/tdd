9f9e08140c66210cebedfd320a4441288bd62706
vf <vf@example.com>
2016-12-26 16:46:40 +0100

    ch6

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -6,7 +6,7 @@ from lists.views import home_page
 import unittest
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -16,11 +16,12 @@ class NewVisitorTest(unittest.TestCase):
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
+        print('===> Checking', row_text)
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retreive_it_later(self):
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
@@ -49,7 +50,3 @@ class NewVisitorTest(unittest.TestCase):
         response = home_page(request)
 
         self.assertIn('A new list item', response.content.decode())
-
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
