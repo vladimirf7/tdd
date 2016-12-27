045bc4201669af6eebbc0a9794655a9ec9ee69bd
vf <vf@example.com>
2016-12-27 14:33:13 +0100

    new URL + view for adding to existing lists. FT passes :-)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -96,6 +96,3 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
-
-        # Satisfied, they both go back to sleep
-        self.fail('Finish the test!')
--- lists/tests.py
+++ lists/tests.py
@@ -70,6 +70,12 @@ class ListViewTest(TestCase):
         response = self.client.get('/lists/%d/' % (list_.id,))
         self.assertTemplateUsed(response, 'list.html')
 
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get('/lists/%d/' % (correct_list.id,))
+        self.assertEqual(response.context['list'], correct_list)
+
 
 
 class NewListTest(TestCase):
