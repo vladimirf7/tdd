5b8c43020c956e7d5ee6ccc8797d8b4eff1b9a41
vf <vf@example.com>
2017-01-08 12:47:14 +0100

    Adjust new list view to do model validation

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,4 +1,6 @@
 from .base import FunctionalTest
+from selenium.webdriver.common.keys import Keys
+import time
 
 
 class ItemValidationTest(FunctionalTest):
@@ -6,16 +8,18 @@ class ItemValidationTest(FunctionalTest):
         # Edith goes to the home page and accidentally tries to submit
         # an empty list item. She hits Enter on the empty input box
         self.browser.get(self.server_url)
-        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank
-        error = self.browser.find_element_by_css_selector('.has-error') #1
+        time.sleep(1)
+        error = self.browser.find_element_by_css_selector('.has-error')
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # She tries again with some text for the item, which now works
-        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
-        self.check_for_row_in_list_table('1: Buy milk') #2
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy milk')
 
         # Perversely, she now decides to submit a second blank list item
         self.browser.find_element_by_id('id_new_item').send_keys('\n')
